5e7fab5c8be01797f0635526adcf9f164b13c098
DPCaraballe <84527568+DPCaraballe@users.noreply.github.com>
2021-06-21 14:49:05 +0200

    Add files via upload

---
+++ opdrachtjavascriptreturn/functions.js
@@ -0,0 +1,59 @@
+
+
+const gymVisitors = function(number) {
+        number = 60;
+        if(number >= 100) {
+            
+            return true; 
+        
+    }
+   
+        if(number <= 100); {
+            
+            return false;
+    }
+    
+};
+
+
+gymVisitors(60);
+
+/* Brenda the bouncer */
+
+const allowedToEnter = (visitorsAge) => {
+
+    
+    if(visitorsAge >= 18); {
+    return "Come in";
+    }
+    if(visitorsAge <= 18); {
+    return "This is a club for adults, sorry";
+    }  
+
+}
+
+allowedToEnter(30);
+console.log();
+
+const clubVisitors = (visitorsAge, maxVisitors, currentVisitors) => {
+    visitorsAge: 18;
+    maxVisitors: 1000;
+    currentVisitors: 800;
+
+    if (visitorsAge >= 18 && maxVisitors <= 1000 && currentVisitors <= 900); {
+    return "Come in";
+    }
+    if (visitorsAge <= 18 && maxVisitors >= 1000 && currentVisitors == 900); {
+    return "I'm sorry you can't enter";
+    }
+   
+
+}
+
+
+/* Calculating the average */ 
+
+const myFunction = [3, 39, 56, 78, 90] {
+
+}
+
